3271909224c63f06826f95d40535bcf5e8d45ec1
Jugunu1101 <nitinnegi7505@gmail.com>
2026-08-02 19:30:44 +0530

    Types of variable in java

---
+++ Varr.java
@@ -0,0 +1,21 @@
+class Varr{
+    public static void main (String []args){
+        int a=78;
+        char ch='h';
+        byte b='0';
+        double d=34455.897;
+        float f=5.7f;
+        short s=45; 
+        boolean bo=true;
+        System.out.println(" int :"+a);
+        System.out.println(" char :"+ch);
+        System.out.println(" byte:"+b);
+        System.out.println(" double :"+d);
+        System.out.println(" float:"+f);
+        System.out.println(" Short  :"+s);
+        System.out.println(" bool :"+bo);
+        System.out.println(" double :"+d);
+       
+
+    }
+}
