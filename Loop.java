d52800706391e37672a9f73734e37abb189acbe5
Jugunu1101 <nitinnegi7505@gmail.com>
2026-08-08 16:18:45 +0530

    h

---
+++ Loop.java
@@ -0,0 +1,16 @@
+class Loop{
+    public static void main (String []args){
+      int i=0;
+      do {
+          i++;
+          if(i==2){
+            System.out.println("i am skiped "+ i);
+            continue ;
+          }
+    System.out.println("hello :" + i);
+      } while(i<5);
+// for loop
+//while loop
+// do while loop
+    }
+}
