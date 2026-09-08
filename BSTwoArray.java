17f7fcea96cbcc5a2d9026ce74ff159d1523ef30
Jugunu1101 <nitinnegi7505@gmail.com>
2026-09-08 23:31:00 +0530

    2D binary s

---
+++ BSTwoArray.java
@@ -0,0 +1,20 @@
+class BSTwoArray{
+    public static void main(String []args){
+        
+    }
+    static int Binary2D(int [][]arr,int target){
+        int r=0;
+        int c=arr.length-1;
+    while(r <arr.length && c>=0){
+        if(arr[r][c]==target){
+            return new int[] {r,c};
+        }
+        if(arr[r][c] < target){
+            r++;
+        }
+        else {
+            c--;
+        }
+    }
+    }
+}
