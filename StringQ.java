0d59b6b0318ec365af00a7293d1e94f46f27e756
Jugunu1101 <nitinnegi7505@gmail.com>
2026-09-11 23:36:55 +0530

    String question

---
+++ StringQ.java
@@ -0,0 +1,9 @@
+class StringQ{
+    public static void main (String []args){
+        String a="Neeraj";
+        String b="Neeraj";
+        
+        System.out.println(a.equal(b));
+        System.out.println("a"+ 'b');
+    }
+}
