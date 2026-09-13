585fa9fd1df780d9d6de821b16a82fd88928a4a3
Jugunu1101 <nitinnegi7505@gmail.com>
2026-09-13 12:54:28 +0530

    print 5 time hlo

--- recursion.java
+++ recursion.java
@@ -11,21 +11,36 @@
 // }
 
 
-
-
 // fibo serise using recursion 
-class recursion {
-    static int fibo(int n){ 
-        int a=0 ;    // 5
-     if(n==0){
-        return 0;
-     }
-     if(n==1){
-        return 1;
-     }
-          return  fibo(n-1) + fibo(n-2);             
+
+// class recursion {
+//     static int fibo(int n){ 
+//         int a=0 ;    // 5
+//      if(n==0){
+//         return 0;
+//      }
+//      if(n==1){
+//         return 1;
+//      }
+//           return  fibo(n-1) + fibo(n-2);             
+//     }
+//     public static void main (String []args){
+//         System.out.print("Fibo serise = "+fibo(8));
+//     }
+// }
+
+
+
+//print hlo 5 time
+class recursion{
+    public static void main(String []args){
+        hloPrint(5);
     }
-    public static void main (String []args){
-        System.out.print("Fibo serise = "+fibo(8));
+    static void hloPrint(int n){
+        if(n<=0){
+            return ;
+        }
+        hloPrint(n-1);
+        System.out.println("Hlo duniya");
     }
 }
