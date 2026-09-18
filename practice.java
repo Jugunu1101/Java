c8d76a755ac3902fb2b0b13ec122e953232c4fb0
Jugunu1101 <nitinnegi7505@gmail.com>
2026-09-18 17:56:54 +0530

    reverse a number

--- practice.java
+++ practice.java
@@ -264,12 +264,12 @@
 class practice {
     public static void main (String []args){
         int n=15;
-        for(int i=2;i<n;i++){
-            if(n%i==0){
+        int i=2;
+       while(i<n){
+           if(n%i==0){
                  System.out.printf(" The  %d is  Not a prime No",i);
-                 System.out.println("");
-                 System.out.println(i);
             }
-        }
+            i++;
+       }
     }
 }
--- recursion.java
+++ recursion.java
@@ -25,22 +25,70 @@
 //           return  fibo(n-1) + fibo(n-2);             
 //     }
 //     public static void main (String []args){
-//         System.out.print("Fibo serise = "+fibo(8));
+//      
+//    System.out.print("Fibo serise = "+fibo(8));
 //     }
 // }
 
 
 
 //print hlo 5 time
+// class recursion{
+//     public static void main(String []args){
+//         hloPrint(5);
+//     }
+//     static void hloPrint(int n){
+//         if(n<=0){
+//             return ;
+//         }
+//         hloPrint(n-1);
+//         System.out.println("Hlo duniya");
+//     }
+// }
+
+
+
+// reverse a number
+// class recursion{
+//     static int rev=0;
+//     static void no_reverse(int no){
+//         if(no==0){
+//             return;
+//         } 
+//         int last_digit=no%10;
+//         rev=rev * 10 +last_digit;
+//         no_reverse(no/10);
+//     }
+//     public static void main(String []args){
+//         int n=1234;
+//         System.out.println(n);
+//         no_reverse(n);
+//         System.out.print(rev);
+//     }
+
+// }
+
+
+
+// array is sorted or not
 class recursion{
-    public static void main(String []args){
-        hloPrint(5);
-    }
-    static void hloPrint(int n){
-        if(n<=0){
+    static boolean ans=false;;
+    static void is_sorted(int []arr,int i){
+        if(i==arr.length-1){
+            ans=true;
             return ;
         }
-        hloPrint(n-1);
-        System.out.println("Hlo duniya");
+        if(arr[i] > arr[i+1] )
+        {
+            return ;
+        }
+        i++;
+        is_sorted(arr,i);
+        
+    }
+    public static void main (String []args){
+        int []arr={1,2,3,5,6,7};
+        is_sorted(arr,0);
+        System.out.print("The Arr is sorted = "+ans);
     }
 }
