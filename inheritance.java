815936b8c137fe7535e20de8275eeb0f75f36192
Jugunu1101 <nitinnegi7505@gmail.com>
2026-09-25 16:58:17 +0530

    inherit another class method

--- inheritance.java
+++ inheritance.java
@@ -21,14 +21,14 @@
 //         System.out.println("Y ="+y);
 //     }
 // }
-// class inheritance {
-//     public static void main (String []args){
-//         child obj=new child();
-//         obj.fun2();
-//         obj.display();
-//     }
-// }
-// //     -<
+class inheritance {
+    public static void main (String []args){
+        child obj=new child();
+          obj.fun2();
+        obj.display(); 
+    }
+}
+
 
 
 
